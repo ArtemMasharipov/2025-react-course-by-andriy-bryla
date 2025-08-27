c1c66d61ccf6dd046033c126f016962bf907812a
Artem Masharipov <112805569+ArtemMasharipov@users.noreply.github.com>
2025-08-27 15:06:33 +0200

    Delete hw-lesson-11/client directory

--- hw-lesson-11/client/src/entities/post/api/postApi.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-import { API_ENDPOINTS } from '../../../shared/config/api'
-
-export const postApi = {
-  list: () => axios.get(API_ENDPOINTS.POSTS),
-}
--- hw-lesson-11/client/src/entities/post/model/selectors.js
+++
@@ -1,3 +0,0 @@
-export const selectPosts = state => state.post.items
-export const selectPostsStatus = state => state.post.status
-export const selectPostsError = state => state.post.error
