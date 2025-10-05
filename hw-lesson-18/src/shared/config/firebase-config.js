d2b491a0570c1c982266f7e7d014c5b846ee03fc
Artem Masharipov <112805569+ArtemMasharipov@users.noreply.github.com>
2025-10-05 18:37:41 +0200

    Update firebase-config.js

--- hw-lesson-18/src/shared/config/firebase-config.js
+++ hw-lesson-18/src/shared/config/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore/lite'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDgjNWXN6OMxkN90r1y-hXxrk1XbvXgqbA",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "hw-lesson-18.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "hw-lesson-18",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "hw-lesson-18.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "587746711479",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:587746711479:web:7b960eba12c6dc49973a32",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
